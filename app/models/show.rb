7c571f0ae049fdaa20d9af2f05253f1da20af5c7
Josh Morris <jmo0624@gmail.com>
2020-01-28 00:30:00 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,11 +1,12 @@
 class Show < ActiveRecord::Base
-  
-  has_many :characters
-  has_many :actors, through: :characters
-  belongs_to :network
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network 
+
   def actors_list
-    self.actor
+    self.characters.collect do |n|
+        n.actor.full_name
+    end
   end
-  
+
 end
